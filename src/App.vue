73a9083f01ddb25989010070c86d8520cde406af
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-05 15:59:59 +0100

    Added Component folder and basic root App with data and template

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedRessources: [
+        {
+          id:'google',
+          name: 'Google',
+          description: 'Google is the main research engine of the planet',
+          link: 'www.google.com'
+        },
+        {
+          id:'vue-style-guide',
+          name: 'Vue Style Guide',
+          description:
+            'Informations to keep your VueJS code as beautiful as possible',
+          link: 'https://v3.vuejs.org/style-guide/'
+        }
+      ]
+    };
+  }
+};
+</script>
