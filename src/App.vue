33987805fab4f75682860cd5ec2f347782402e0e
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-05 17:05:30 +0100

    Added scoped CSS to component and adjusted global one

--- src/App.vue
+++ src/App.vue
@@ -37,11 +37,14 @@ export default {
 </script>
 
 <style>
-*{
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+
+* {
   box-sizing: border-box;
 }
+
 html {
-  font-family: Helvetica;
+  font-family: 'Roboto', sans-serif;
 }
 
 body {
