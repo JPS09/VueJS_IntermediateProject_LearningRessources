ce570f53a5864bed3dd68704f0a6a8b711897f65
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-05 20:30:29 +0100

    Structure of tabs feature

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,22 @@
+<template>
+  <base-card>
+    <base-button @click="tabSelect('stored-resources')">Stored Resources</base-button>
+    <base-button @click="tabSelect('add-resource')">Create a Resource</base-button>
+    <component :is="selectedTab"></component>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedTab: 'stored-resources'
+    };
+  },
+  methods: {
+    tabSelect(tab) {
+      this.selectedTab = tab;
+    }
+  }
+};
+</script>
