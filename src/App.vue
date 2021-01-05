13366c00eab6a109c7d1da4a7562f5b20b00bda1
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-05 16:22:38 +0100

    Adding a component to display list of ressources

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,31 @@
 <template>
-  <ul></ul>
+  <base-item
+    v-for="ressource in storedRessources"
+    :key="ressource.id"
+    :name="ressource.name"
+    :id="ressource.id"
+    :description="ressource.description"
+    :link="ressource.link"
+  ></base-item>
 </template>
 
 <script>
+import BaseItem from './components/BaseItem.vue';
 export default {
+  components: {
+    BaseItem
+  },
   data() {
     return {
       storedRessources: [
         {
-          id:'google',
+          id: 'google',
           name: 'Google',
           description: 'Google is the main research engine of the planet',
           link: 'www.google.com'
         },
         {
-          id:'vue-style-guide',
+          id: 'vue-style-guide',
           name: 'Vue Style Guide',
           description:
             'Informations to keep your VueJS code as beautiful as possible',
---
+++ src/components/BaseItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <ul>
+    <li >
+      <h3>{{ name }}</h3>
+      <p>{{ description }}</p>
+      <a :href="link" target="_blank">{{ link }}</a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true
+    },
+    name: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    link: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
