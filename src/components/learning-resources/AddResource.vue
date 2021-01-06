f7c762b215e79405926965dc6d75ae64345d51ce
Jean Pierre Symoens <jpsymoens0@gmail.com>
2021-01-06 10:22:51 +0100

    Added divs and adjusted id for semantics, changed input to text area for description

--- src/components/learning-resources/AddResource.vue
+++ src/components/learning-resources/AddResource.vue
@@ -1,22 +1,36 @@
 <template>
   <h2>Add a resource</h2>
-  <form>
-    <label for="name">Resource Title</label>
-    <input type="text" name="name" placeholder="Enter the resource name" />
-    <label for="description">Description</label>
-    <input
-      type="text"
-      name="description"
-      placeholder="What does this resource gives?"
-    />
-    <label for="link">Link</label>
-    <input type="text" name="link" placeholder="Link" />
-    <button @click.prevent>Add</button>
-  </form>
+  <base-card>
+    <form>
+      <div class="form-control">
+        <label for="title">Resource Title</label>
+        <input
+          type="text"
+          id="title"
+          name="title"
+          placeholder="Enter the resource name"
+        />
+      </div>
+      <div class="form-control">
+        <label for="description">Description</label>
+        <textarea
+          type="text"
+          id="description"
+          name="description"
+          placeholder="What does this resource gives?"
+        />
+      </div>
+      <div class="form-control">
+        <label for="link">Link</label>
+        <input type="text" name="link" id="link" placeholder="Link" />
+      </div>
+      <button @click.prevent>Add</button>
+    </form>
+  </base-card>
 </template>
 
 <style scoped>
 form {
-  display: grid;
+
 }
 </style>
